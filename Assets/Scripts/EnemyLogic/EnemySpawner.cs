225877dce571c1514ae4441027318f84217666b1
OVOAOVO <2410651402@qq.com>
2025-04-25 10:52:26 +0800

    Collider wait one frame

--- Assets/Scripts/EnemyLogic/EnemySpawner.cs
+++ Assets/Scripts/EnemyLogic/EnemySpawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections;
 using MoreMountains.Feedbacks;
 using MoreMountains.Tools;
+
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject monsterPrefab;
@@ -16,13 +17,7 @@ public class EnemySpawner : MonoBehaviour
     public MMProgressBar progressBar;
     private float previousHit = 0f;
 
-    // LateUpdate
-    //同一帧里“生成”一个敌人，再马上做一次 Physics.Raycast，新的 Collider 已经被注册到场景里了；
-    // 如果它恰巧落在你的射线方向上，就会被立刻击中，造成一种穿透并且击中了刚生成敌人的bug
-    // “前面生成的敌人被穿透”，但可能你认为“先生成的”就是“视觉上靠近摄像机的”。
-    // 但其实 Unity 并不会自动把“先生成的对象”摆在“前面”。
-    // 如果你在一个半圆范围内随机生成敌人，他们的位置是随机的，哪怕一个敌人在逻辑上“先生成”，但空间位置上可能在队尾，Raycast 命中它也合理。
-    void LateUpdate()
+    void Update()
     {
         if (Conductor.Instance.hit > previousHit)
         {
@@ -42,8 +37,27 @@ public class EnemySpawner : MonoBehaviour
         monsterObj.transform.position = spawnPos;
         monsterObj.transform.LookAt(target.transform);
 
+        // 先关闭 Collider
+        Collider col = monsterObj.GetComponent<Collider>();
+        if (col != null) col.enabled = false;
+
+        // 下一帧再启用 Collider
+        StartCoroutine(EnableColliderNextFrame(col));
+
         Enemy enemy = monsterObj.GetComponent<Enemy>();
         enemy.Initialize(target.transform, moveSpeed, OnEnemyReached);
+    }
+
+    // NOTE:
+    // 同一帧里“生成”一个敌人，再马上做一次 Physics.Raycast，新的 Collider 已经被注册到场景里了；
+    // 不管在任何方向上？在当前方向上？，就会被立刻击中，造成一种穿透并且击中了刚生成敌人的bug
+    // “前面生成的敌人被穿透”，但可能你认为“先生成的”就是“视觉上靠近摄像机的”。
+    // 但其实 Unity 并不会自动把“先生成的对象”摆在“前面”。
+    // 如果你在一个半圆范围内随机生成敌人，他们的位置是随机的，哪怕一个敌人在逻辑上“先生成”，但空间位置上可能在队尾，Raycast 命中它也合理。
+    IEnumerator EnableColliderNextFrame(Collider col)
+    {
+        yield return null; // 等待一帧
+        if (col != null) col.enabled = true;
     }
 
     void OnEnemyReached(Enemy enemy)
