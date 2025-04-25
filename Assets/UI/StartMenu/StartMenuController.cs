d3292900ca189b1167a1312d11a39bad55a9746c
OVOAOVO <2410651402@qq.com>
2025-04-25 18:20:11 +0800

    StartMenu

--- Assets/UI/StartMenu/StartMenuController.cs
+++ Assets/UI/StartMenu/StartMenuController.cs
@@ -1,8 +1,10 @@
+using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class StartMenuController : MonoBehaviour
 {
+    public MMFeedbacks feedbacks; // 反馈系统
     void OnEnable()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
@@ -14,6 +16,7 @@ public class StartMenuController : MonoBehaviour
             Debug.Log("进入游戏！");
             // TODO: 切换场景或执行逻辑
             // SceneManager.LoadScene("MainGameScene");
+            feedbacks.PlayFeedbacks(); // 播放反馈
         };
     }
 }
