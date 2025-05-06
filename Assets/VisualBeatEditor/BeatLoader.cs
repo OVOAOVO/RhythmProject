6f219d9908d6ffcecbc15527842c4e887d24ba4a
OVOAOVO <2410651402@qq.com>
2025-05-06 15:47:50 +0800

    LoaderFix

--- Assets/VisualBeatEditor/BeatLoader.cs
+++ Assets/VisualBeatEditor/BeatLoader.cs
@@ -22,11 +22,7 @@ public class BeatLoader : MonoBehaviour
     {
         string filePath;
 
-    #if UNITY_EDITOR
         filePath = Path.Combine(Application.dataPath, $"BeatBook/{mapName}.json");
-    #else
-        filePath = Path.Combine(Application.dataPath, $"../{mapName}.json");
-    #endif
 
         Debug.Log($"🧾 Attempting to load beat data: {filePath}");
 
